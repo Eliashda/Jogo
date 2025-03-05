338014572a6fc07b13f0556a3bc05332979a837d
ELIAS ALMENDA RODRIGUES PADILHA <elias_a_padilha@estudante.sesisenai.org.br>
2025-03-05 09:19:37 -0300

    inicializando js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-a=1
+// pegar o elemento canvas pelo id
+const canvas = document.getElementById('jogo2d')
+// inicializar o canvas
+const ctx = canvas.getContext('2d')
+
+// função que seja executada 60hz loop
+function loop () {
+
+}
+
+
+loop()
